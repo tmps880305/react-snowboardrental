1919366e295d9e0f1076729cfc80d85a77fdcb4f
randy <tmps880305@gmail.com>
2022-11-30 00:54:20 -0500

    Create bump feedback of the cart button when adding items to the cart, applying useEffect.

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState, useEffect} from 'react';
 
 import classes from './HeaderCartButton.module.css';
 import CartIcon from '../Cart/CartIcon';
@@ -6,15 +6,35 @@ import CartContext from '../../store/cart-content';
 
 const HeaderCartButton = (props) => {
 
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext);
 
-    const numberOfCartItems = cartCtx.items.reduce((accumulator, item) => {
+    const {items} = cartCtx;
+
+    const numberOfCartItems = items.reduce((accumulator, item) => {
         return accumulator + item.amount;
     }, 0);
 
+    useEffect(() => {
+        if (items.length === 0) {
+            return
+        }
+        setBtnIsHighlighted(true);
+
+        const timer = setTimeout(() => {
+            setBtnIsHighlighted(false);
+        }, 300);
+
+        return () => {
+            clearTimeout(timer);
+        }
+
+    }, [items]);
+
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
 
     return (
-        <button className={classes.button} onClick={props.onClick}>
+        <button className={btnClasses} onClick={props.onClick}>
             <span className={classes.icon}>
                 <CartIcon/>
             </span>
