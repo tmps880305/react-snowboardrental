37094598680fd0c95cdff71fa7da999bf338e334
randy <tmps880305@gmail.com>
2022-12-06 18:35:01 -0500

    Section 17-214. Moving "Meals" Data To The Backend
    My solution

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,38 +1,39 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
 import MealItem from './MealItem/MealItem';
+import useHttp from '../../hooks/use-http';
 
-const DUMMY_MEALS = [
-    {
-        id: 'm1',
-        name: 'Sushi',
-        description: 'Finest fish and veggies',
-        price: 22.99,
-    },
-    {
-        id: 'm2',
-        name: 'Schnitzel',
-        description: 'A german specialty!',
-        price: 16.5,
-    },
-    {
-        id: 'm3',
-        name: 'Barbecue Burger',
-        description: 'American, raw, meaty',
-        price: 12.99,
-    },
-    {
-        id: 'm4',
-        name: 'Green Bowl',
-        description: 'Healthy...and green...',
-        price: 18.99,
-    },
-];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => {
+    const [meals, setMeals] = useState([]);
+
+    const {isLoading, error, sendRequest: fetchMovieHandler} = useHttp();
+
+    const fetchData = () => {
+        const requestMoviesConfig = {url: 'https://react-http-aa7a6-default-rtdb.firebaseio.com/meal.json'};
+        const transformMeals = (mealsObj) => {
+            const loadMeals = [];
+            for (const key in (mealsObj)) {
+                loadMeals.push({
+                    id: key,
+                    name: mealsObj[key].name,
+                    description: mealsObj[key].description,
+                    price: mealsObj[key].price
+                });
+            }
+            setMeals(loadMeals);
+        };
+        fetchMovieHandler(requestMoviesConfig, transformMeals);
+    };
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+
+    const mealsList = meals.map(meal => {
         return <MealItem
             key={meal.id}
             id={meal.id}
@@ -42,15 +43,24 @@ const AvailableMeals = () => {
         />
     });
 
+    let content = <p>Found no meals.</p>;
+    if (error) {
+        content = <p>{error}</p>
+    }
+    if (mealsList.length > 0) {
+        content = <ul>{mealsList}</ul>
+    }
+    if (isLoading) {
+        content = <p>Meal is loading...</p>
+    }
+
     return (
         <section className={classes.meals}>
             <Card>
-                <ul>{mealsList}</ul>
+                {content}
             </Card>
         </section>
-
     )
-
 };
 
 export default AvailableMeals;
---
+++ src/hooks/use-http.js
@@ -0,0 +1,42 @@
+import React, {useState, useCallback} from 'react';
+
+const useHttp = () => {
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const sendRequest = useCallback(async (httpRequestConfig, applyData) => {
+        setIsLoading(true);
+        setError(null);
+        try {
+            const response = await fetch(httpRequestConfig.url, {
+                method: httpRequestConfig.method ? httpRequestConfig.method : 'GET',
+                headers: httpRequestConfig.headers ? httpRequestConfig.headers : {},
+                body: httpRequestConfig.body ? JSON.stringify(httpRequestConfig.body) : null
+            });
+
+            if (!response.ok) {
+                throw new Error(`Error: ${response.status}`);
+            }
+
+            const data = await response.json();
+            applyData(data);
+
+        } catch (error) {
+            setError(error.message || 'Something went wrong.');
+        }
+        setIsLoading(false);
+    }, []);
+
+    return {
+        isLoading,
+        error,
+        sendRequest
+
+        // Same as:
+        // isLoading: isLoading,
+        // error: error,
+        // sendRequest: sendRequest
+    }
+};
+
+export default useHttp;
